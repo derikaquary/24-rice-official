53463fcb137116bfcd8ee7a4defc4bac92ccfab8
derikaquary <derikaquary@gmail.com>
2024-06-29 12:14:08 +0800

    creating the about us background

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,19 @@
+import Image from "next/image";
+import bgabout from "@/public/bg-about.jpg";
+
 function page() {
-  return <div>About</div>;
+  return (
+    <div className="relative z-[-10] h-screen">
+      <Image
+        src={bgabout}
+        fill
+        alt="rice field"
+        placeholder="blur"
+        quality={100}
+        className="mt-[146px] object-cover object-top sm:mt-[120px] md:mt-[122px] lg:mt-[3rem]"
+      />
+    </div>
+  );
 }
 
 export default page;
