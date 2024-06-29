f962ba1ddcccd2e3e7ac196188107cfa40645883
derikaquary <derikaquary@gmail.com>
2024-06-29 14:59:14 +0800

    creating InfoAbout component

---
+++ app/_components/InfoAbout.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+
+function InfoAbout({ src, alt, children }) {
+  return (
+    <>
+      <div className="relative aspect-square h-[300px]">
+        <Image
+          src={src}
+          fill
+          alt={alt}
+          placeholder="blur"
+          quality={100}
+          className="object-cover"
+        />
+      </div>
+      {children}
+    </>
+  );
+}
+
+export default InfoAbout;
